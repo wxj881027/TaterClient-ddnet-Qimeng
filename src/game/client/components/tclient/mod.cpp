bff4417e226b512c19d587ef96cc091429a22864
SollyBunny <sollybunn@gmail.com>
2025-05-18 22:14:42 +0000

    Allow ip in mod_rcon_xxx

--- src/game/client/components/tclient/mod.cpp
+++ src/game/client/components/tclient/mod.cpp
@@ -5,6 +5,7 @@
 #include <base/system.h>
 
 #include "mod.h"
+#include "engine/shared/protocol.h"
 
 int CMod::IdFromName(const char *pName)
 {
@@ -54,15 +55,27 @@ int CMod::TimeFromStr(const char *pStr, char OutUnit)
 
 void CMod::ConModCmd(IConsole::IResult *pResult, const char *pCmd, bool IsName, char Unit)
 {
-	int ClientId;
+	const char *pClient = nullptr;
 	if(IsName)
-		ClientId = IdFromName(pResult->GetString(0));
-	else
-		ClientId = pResult->GetInteger(0);
-	if(ClientId < 0)
 	{
-		GameClient()->Echo("Invalid id/name");
-		return;
+		int ClientId = IdFromName(pResult->GetString(0));
+		if(ClientId < 0 || ClientId > SERVERINFO_MAX_CLIENTS)
+		{
+			GameClient()->Echo("Invalid id, out of range");
+			return;
+		}
+		if(!GameClient()->m_Snap.m_aCharacters[ClientId].m_Active)
+		{
+			GameClient()->Echo("Invalid id, no player with this id");
+			return;
+		}
+		char aBuf[32];
+		str_format(aBuf, sizeof(aBuf), "%d", ClientId);
+		pClient = aBuf;
+	}
+	else
+	{
+		pClient = pResult->GetString(0); // Could be ID or IP
 	}
 	int Time = TimeFromStr(pResult->GetString(1), Unit);
 	if(Time < 0)
@@ -72,7 +85,7 @@ void CMod::ConModCmd(IConsole::IResult *pResult, const char *pCmd, bool IsName, 
 	}
 
 	char aBuf[256];
-	str_format(aBuf, sizeof(aBuf), "%s %d %d %s", pCmd, ClientId, Time, pResult->GetString(2));
+	str_format(aBuf, sizeof(aBuf), "%s %s %d %s", pCmd, pClient, Time, pResult->GetString(2));
 	Client()->Rcon(aBuf);
 }
 
