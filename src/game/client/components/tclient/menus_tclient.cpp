ef8910e328c1d6a465c6771cd6944456e7b4d578
SollyBunny <sollybunn@gmail.com>
2025-06-02 20:59:43 +0000

    Remove tc_freeze_update_fix, in DDNet by default

--- src/game/client/components/tclient/menus_tclient.cpp
+++ src/game/client/components/tclient/menus_tclient.cpp
@@ -639,7 +639,6 @@ void CMenus::RenderSettingsTClientSettngs(CUIRect MainView)
 	else
 		Ui()->DoScrollbarOption(&g_Config.m_ClAnimateWheelTime, &g_Config.m_ClAnimateWheelTime, &Button, TCLocalize("Wheel animate"), 0, 1000, &CUi::ms_LinearScrollbarScale, 0, "ms (off)");
 
-	DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_ClFreezeUpdateFix, TCLocalize("Update tee skin faster after being frozen"), &g_Config.m_ClFreezeUpdateFix, &Column, LineSize);
 	DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_ClPingNameCircle, TCLocalize("Show ping colored circle before names"), &g_Config.m_ClPingNameCircle, &Column, LineSize);
 	DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_ClRenderNameplateSpec, TCLocalize("Hide nameplates in spec"), &g_Config.m_ClRenderNameplateSpec, &Column, LineSize);
 	DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_ClNameplatesSkinInfo, TCLocalize("Shows skin name and colors in nameplates"), &g_Config.m_ClNameplatesSkinInfo, &Column, LineSize);
